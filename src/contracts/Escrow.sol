a069471a869ade0b03c109251322546b90e85e75
george-shammar <george.gbenle@outlook.com>
2021-07-03 22:34:27 +0000

    Update project

--- src/contracts/Escrow.sol
+++ src/contracts/Escrow.sol
@@ -1,2 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+
+contract Escrow {
+    
+    address agent;
+    
+    mapping(address => uint256) public deposits;
+    
+    modifier onlyAgent() {
+        require(msg.sender == agent);
+        _;
+    }
+    
+    constructor() {
+        agent = msg.sender;
+    }
+    
+     function debitBuyer() public payable {
+        address payee = agent;
+        uint256 buyingAmount = msg.value;
+        deposits[payee] = deposits[payee] + buyingAmount;
+    }
+    
+    function creditEscrow(address payable payee) public payable {
+        uint256 credit = deposits[payee];
+        deposits[payee] = 0;
+        payee.transfer(credit);
+    }
+    
+    function debitEscrow(address payee) public onlyAgent payable {
+        uint256 amount = msg.value;
+        
+        deposits[payee] = deposits[payee] + amount;
+    }
+    
+    function creditSeller(address payable payee) public onlyAgent payable {
+        uint256 payment = deposits[payee];
+        deposits[payee] = 0;
+        payee.transfer(payment);
+    } 
+}
